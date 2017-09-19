689297677f17c4e05d9210ce7e57b64cd68422f2
LessCodeToDoMore <2506505996@qq.com>
2017-09-19 22:48:10 +0800

    2017-9-19 22

--- application/database.php
+++ application/database.php
@@ -16,7 +16,7 @@ return [
     // 服务器地址
     'hostname'        => 'localhost',
     // 数据库名
-    'database'        => 'fastadmin4',
+    'database'        => 'fastadmin',
     // 用户名
     'username'        => 'root',
     // 密码
